e2d1d0d0c7b4ce1f668ed98782d4826a6295e985
s133996 <mit.navn.marcus@jubii.dk>
2017-10-12 13:48:00 +0200

    Add files via upload
    rettelse af rolls randomness og ændring i rollMultiple for at kaste det antal gange bruger skriver ind.
    Samt opdatering af RollMultipleTest

--- RollMultipleTest.java
+++ RollMultipleTest.java
@@ -33,12 +33,12 @@ public class RollMultipleTest {
 		System.out.println(rollArray[4] + " times was a 5 rolled");
 		System.out.println(rollArray[5] + " times was a 6 rolled");
 		
-		boolean actual0 = 59600 < rollArray[0] && rollArray[0] < 60400;
-		boolean actual1 = 59600 < rollArray[1] && rollArray[1] < 60400;
-		boolean actual2 = 59600 < rollArray[2] && rollArray[2] < 60400;
-		boolean actual3 = 59600 < rollArray[3] && rollArray[3] < 60400;
-		boolean actual4 = 59600 < rollArray[4] && rollArray[4] < 60400;
-		boolean actual5 = 59600 < rollArray[5] && rollArray[5] < 60400;
+		boolean actual0 = 9600 < rollArray[0] && rollArray[0] < 10400;
+		boolean actual1 = 9600 < rollArray[1] && rollArray[1] < 10400;
+		boolean actual2 = 9600 < rollArray[2] && rollArray[2] < 10400;
+		boolean actual3 = 9600 < rollArray[3] && rollArray[3] < 10400;
+		boolean actual4 = 9600 < rollArray[4] && rollArray[4] < 10400;
+		boolean actual5 = 9600 < rollArray[5] && rollArray[5] < 10400;
 		boolean actual = actual0 && actual1 && actual2 && actual3 && actual4 && actual5;
 		assertEquals(true, actual);
 	}
